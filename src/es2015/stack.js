b52541337e3be9953475e3c685e37ba4133d3a12
Jainish <jainishjain@gmail.com>
2018-01-22 17:10:23 +0530

    Implemented ES2015 class pattern for Stack

--- src/es2015/stack.js
+++ src/es2015/stack.js
@@ -1,4 +1,26 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+  }
 
+  push(value) {
+    this[this.top += 1] = value;
+  };
+
+  pop() {
+    if(this.top !== -1) {
+      const val = this[this.top];
+      delete this[this.top];
+      this.top -= 1;
+      return val;
+    }
+    return undefined;
+  };
+
+  size() {
+    return this.top + 1;
+  };
+}
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = Stack;
