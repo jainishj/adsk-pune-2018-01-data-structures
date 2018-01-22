a659b6724fcc905971174be3f376186f5c80be4e
Jainish <jainishjain@gmail.com>
2018-01-22 15:37:42 +0530

    Queue using Functional pattern

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -1,18 +1,29 @@
 const Queue = function() {
-  const someInstance = {};
-
+  const someInstance = {
+  };
   // Use an object with numeric keys to store values
-  const storage = {};
+  const storage = {
+    rear:0,
+    front:0
+  };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[storage.rear] = value;
+    storage.rear += 1;
   };
 
   someInstance.dequeue = function() {
+    let val = storage[storage.front];
+    delete storage[storage.front];
+    storage.front += 1;
+    return val;
   };
 
   someInstance.size = function() {
+    let len = storage.rear - storage.front;
+    return len < 0 ? 0 : len;
   };
 
   return someInstance;
