f342cfff1f39aa360035be5864b78c43d2961c3a
Jainish <jainishjain@gmail.com>
2018-01-22 17:08:27 +0530

    Implemented ES2015 class pattern for Queue

--- src/es2015/queue.js
+++ src/es2015/queue.js
@@ -1,4 +1,28 @@
+class Queue {
+  constructor() {
+    this.rear = 0;
+    this.front = 0;
+  }
 
+  enqueue(value) {
+    this[this.rear] = value;
+    this.rear += 1;
+  }
+
+  dequeue() {
+    if(this.size() > 0) {
+      const val = this[this.front];
+      delete this[this.front];
+      this.front += 1;
+      return val;
+    }
+    return undefined;
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = Queue;
