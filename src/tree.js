33d02c55a380f5bdfcd04b6a3449e81c172807b5
Jainish <jainishjain@gmail.com>
2018-01-22 20:12:14 +0530

    Tree implementation

--- src/tree.js
+++ src/tree.js
@@ -5,20 +5,22 @@ const Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 const treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  return _.reduce(this.children, function(result, node) {
+    return result || (node.value === target) || node.contains(target);
+  }, false);
 };
-
-
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = Tree;
