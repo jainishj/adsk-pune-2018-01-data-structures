a2f064a5451226487a8bececad9cee4cb1aba990
Jainish <jainishjain@gmail.com>
2018-01-22 16:17:39 +0530

    Implemented Functional-shared pattern for queue

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,13 +1,36 @@
 const _ = require('underscore');
 
 const Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  const storage = {
+    rear:0,
+    front:0
+  };
+
+  Object.assign(storage, queueMethods);
+
+  return storage;
 };
 
-const queueMethods = {};
+const queueMethods = {
+  enqueue: function(value) {
+    this[this.rear]  = value;
+    this.rear += 1;
+  },
 
+  dequeue: function() {
+    if(this.size() > 0) {
+      const val = this[this.front];
+      delete this[this.front];
+      this.front += 1;
+      return val;
+    }
+    return undefined;
+  },
 
+  size: function() {
+    return this.rear - this.front;
+  }
+};
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = Queue;
