b573315cd9ae4fe2985eeaecd5bcc62e40529950
Jainish <jainishjain@gmail.com>
2018-01-22 16:39:25 +0530

    Implemented Pseudoclassical pattern for Queue

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,8 +1,30 @@
 const Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.rear = 0;
+  this.front = 0;
 };
 
+Queue.prototype = {
+  enqueue: function(value) {
+    this[this.rear]  = value;
+    this.rear += 1;
+  },
+
+  dequeue: function() {
+    if(this.size() > 0) {
+      const val = this[this.front];
+      delete this[this.front];
+      this.front += 1;
+      return val;
+    }
+    return undefined;
+  },
+
+  size: function() {
+    return this.rear - this.front;
+  }
+}
+
+Queue.prototype.constructor = Queue;
 
 
 if (typeof module !== 'undefined' && module.exports) {
