7c710f59719b966818a22b43da35dab0a1654efb
Jainish <jainishjain@gmail.com>
2018-01-22 16:56:48 +0530

    Implemented Pseudoclassical pattern for Stack

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,9 +1,24 @@
 const Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = -1;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.top += 1] = value;
+};
 
+Stack.prototype.pop = function() {
+  if(this.size() > 0) {
+    let value = this[this.top];
+    delete this[this.top];
+    this.top -= 1;
+    return value;
+  }
+  return undefined;
+};
+
+Stack.prototype.size = function() {
+  return this.top + 1;
+};
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = Stack;
